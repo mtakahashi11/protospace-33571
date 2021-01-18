29bbfc4c6f99817377010872dbe8ac15e2964fec
mtakahashi11 <76547704+mtakahashi11@users.noreply.github.com>
2021-01-18 11:57:30 +0900

    トップページへの遷移機能の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
-  before_action :authenticate_user!, except: [:edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @prototypes = Prototype.all
